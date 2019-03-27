522fed20230065016d578f96583519e92ab2d892
yashasjoshi <yashasjoshi1996@gmail.com>
2019-03-27 13:09:21 -0400

    First Commit

---
+++ Assets/Scripts/GlowBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowBehaviour : MonoBehaviour {
+
+    public Material glowMaterial;
+
+    private Renderer myRenderer;
+    private Material normalMaterial;
+
+	// Use this for initialization
+	void Start () {
+        this.myRenderer = GetComponent<Renderer>();
+        this.normalMaterial = this.myRenderer.material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Highlight()
+    {
+        myRenderer.material = glowMaterial;
+    }
+
+    public void NormalRendering()
+    {
+        myRenderer.material = normalMaterial;
+    }
+}
---
+++ Assets/Scripts/VREventHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRStandardAssets.Utils;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(VRInteractiveItem))]
+public class VREventHandler : MonoBehaviour {
+
+    private VRInteractiveItem interactiveItem;
+    public UnityEvent GazeEnterEvent;
+    public UnityEvent GazeExitEvent;
+
+	// Use this for initialization
+	void Start () {
+        interactiveItem = GetComponent<VRInteractiveItem>();
+        interactiveItem.OnOver += OnGazeEnter;
+        interactiveItem.OnOut += OnGazeExit;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnGazeEnter()
+    {
+        GazeEnterEvent.Invoke();
+    }
+
+    void OnGazeExit()
+    {
+        GazeExitEvent.Invoke();
+    }
+}
